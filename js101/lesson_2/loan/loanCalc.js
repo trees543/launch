ed033c6513442e2aeb0f8c755e082feb0381037c
trevor rj <treesjones3@gmail.com>
2023-02-05 14:10:14 -0600

    final loan calculator

--- js101/lesson_2/loan/loanCalc.js
+++ js101/lesson_2/loan/loanCalc.js
@@ -1,23 +1,24 @@
+//ON NEW BRANCH TEST
+//Need to prevent allowing decimals
+
 const readline = require('readline-sync');
-const MESSAGES = require('./messages.json')
+const MESSAGES = require('./messages.json');
 
-console.log('Welcome to the loan calculator!');
-
-const loan = {
-	loanAmount: null,
-	apr: null,
-	loanDurationInYears: null,
-	additionalMonthsOnLoanDuration: null
-}
-
-function isInvalidValue(val) {
-  return Number.isNaN(Number(val)) || // handles strings
-		val.trim() === '' || // handles empty string converting to 0
-		Number(val) < 0; // handles negative numbers
-}
+console.log(MESSAGES.welcome);
 
 function prompt(message) {
   return readline.question(`=> ${message}\n`);
+}
+
+function isInvalidValue(val, requireWholeNumber=false) {
+  return Number.isNaN(Number(val)) ||
+		val.trim() === '' ||
+		Number(val) < 0 ||
+    (requireWholeNumber ? !isWholeNumber(val) : false);
+}
+
+function isWholeNumber(val) {
+	return val % 1 === 0
 }
 
 function convertToMonthlyRate(rate) {
@@ -29,66 +30,90 @@ function convertFromYearsToMonths(years, months) {
 }
 
 function convertFromNumberToPercentage(num) {
-	return Number(num) / 100
+  return Number(num) / 100;
 }
 
 function formatPayment(pmt) {
-	return `$${pmt.toFixed(2)}`
+  return `$${pmt.toFixed(2)}`;
 }
 
-function newCalculation() {
-	let requestAnotherCalculation = prompt(MESSAGES.anotherCalculation);
-	if (requestAnotherCalculation[0].toLowerCase() !== 'y') {
-		return false;
-	} else return true;
+function newCalculationRequested() {
+  let requestAnotherCalculation = prompt(MESSAGES.anotherCalculation);
+
+  if (requestAnotherCalculation[0].toLowerCase() !== 'y') {
+    return false;
+  } else return true;
 }
 
-function askQuestion(item) {
-	let val = prompt(MESSAGES[item]);
-	while(isInvalidValue(val)) {
-		val = prompt(MESSAGES.invalid);
-	}
-	loan[item] = val
+function getUserInput(loanItem, loanObj) {
+  const { 
+    item, 
+    message, 
+    requireWholeNumber
+  } = loanItem;
+
+  let val = prompt(message);
+
+  while (isInvalidValue(val, requireWholeNumber)) {
+    let invalidMessage = requireWholeNumber ? MESSAGES.invalid2 : MESSAGES.invalid1
+    val = prompt(invalidMessage);
+  }
+
+  loanObj[item] = val;
 }
 
+function calculateMonthlyPayment(loan) {
+  const { 
+    loanAmount,
+    monthlyInterestRate,
+    loanDurationInMonths
+  } = loan;
 
-
-while (true) {
-	// Ask user for loan amount
-		// Check if loan amount is a number that is positive
-		askQuestion('loanAmount');
-		
-		//Ask user for apr
-			// Check if apr is a number that is positive
-		askQuestion('apr');
-		
-		// Ask user for loan duration
-			// Check if loan duration is a number that is positive
-		askQuestion('loanDurationInYears');
-		
-		askQuestion('additionalMonthsForLoanDuration');
-		//Convert interest rate to percentage
-		loan.apr = convertFromNumberToPercentage(loan.apr)
-		// Convert interest rate to monthly
-		let monthlyInterestRate = convertToMonthlyRate(loan.apr);
-		// Convert loan duration to monthly
-		let loanDurationInMonths = convertFromYearsToMonths(loan.loanDurationInYears, loan.additionalMonthsOnLoanDuration);
-		
-		//Once all values received and converted, use the formula
-		function calculateMonthlyPayment() {
-			const { loanAmount } = loan;
-			return loanAmount * 
-				(monthlyInterestRate / 
-					(1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths)))
-				);
-		}
-		
-		let monthlyPayment = calculateMonthlyPayment();
-		
-		console.log(`Monthly Loan Payment of: ${formatPayment(monthlyPayment)}`);
-
-		if (!newCalculation()) break;
+  return loanAmount *
+		(monthlyInterestRate /
+			(1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths)))
+		);
 }
 
+do {
 
+  const loan = {};
 
+  [
+    {
+      item: 'loanAmount',
+      message: MESSAGES.loanAmount,
+      requireWholeNumber: false
+    },
+    {
+      item: 'apr',
+      message: MESSAGES.apr,
+      requireWholeNumber: false
+    },
+    {
+      item: 'loanDurationInYears',
+      message: MESSAGES.loanDurationInYears,
+      requireWholeNumber: true
+    },
+    {
+      item: 'additionalMonthsForLoanDuration',
+      message: MESSAGES.additionalMonthsForLoanDuration,
+      requireWholeNumber: true
+    }
+  ].forEach(loanItem => {
+    getUserInput(loanItem, loan);
+  });
+
+  loan["aprPercentage"] = convertFromNumberToPercentage(loan.apr);
+
+  loan["monthlyInterestRate"] = convertToMonthlyRate(loan.aprPercentage);
+
+  loan["loanDurationInMonths"] = convertFromYearsToMonths(loan.loanDurationInYears, loan.additionalMonthsForLoanDuration);
+
+  let monthlyPayment = calculateMonthlyPayment(loan);
+
+  console.log(`Monthly Loan Payment of: ${formatPayment(monthlyPayment)}`);
+
+} while (newCalculationRequested());
+
+//Questions - ok to recreate new objects (bad for memory, no?); is this getting too cute? 
