368ce897e8bf78b3a0037f315bb865e84890fc8e
trevor rj <treesjones3@gmail.com>
2023-02-11 18:32:19 -0500

    spock lizard complete

--- js101/lesson_2/rps/rps.js
+++ js101/lesson_2/rps/rps.js
@@ -1,67 +1,129 @@
 const readline = require('readline-sync');
 const MESSAGES = require('./messages.json');
 
-const MOVE_OPTIONS = ['rock', 'paper', 'scissors'];
+const MOVE_OPTIONS = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
 function prompt(message) {
-	return readline.question(`=> ${message}\n`);
-};
+  return readline.question(`=> ${message}\n`);
+}
 
 function getUsersChoice(message) {
-	return prompt(`${message}${MOVE_OPTIONS.join(' | ')}`);
-};
+  return prompt(`${message}${MOVE_OPTIONS.join(' | ')}`);
+}
 
 function getRandomInt() {
-	return Math.floor(Math.random() * MOVE_OPTIONS.length);
-};
+  return Math.floor(Math.random() * MOVE_OPTIONS.length);
+}
 
 function getCompChoice() {
-	let randomIndex = getRandomInt(0, 2);
-	return MOVE_OPTIONS[randomIndex];
+  let randomIndex = getRandomInt();
+  return MOVE_OPTIONS[randomIndex];
 }
 
 function isInvalid(selection) {
-	return !MOVE_OPTIONS.includes(selection);
-};
+  return !MOVE_OPTIONS.includes(selection);
+}
 
-function determineWinner(uChoice, cChoice) {
-	if (uChoice === cChoice) {
-		return 'Tie!'
-	} else if (
-		(uChoice === 'rock' && cChoice === 'scissors') ||
-		(uChoice === 'paper' && cChoice === 'rock') ||
-		(uChoice === 'scissors' && cChoice === 'paper')
-	) {
-		return 'You win!'
-	} else return 'Computer wins...'
+function userWins(uChoice, cChoice) {
+  return (uChoice === 'rock' && ['scissors', 'lizard'].includes(cChoice)) ||
+  (uChoice === 'paper' && ['rock', 'spock'].includes(cChoice)) ||
+  (uChoice === 'scissors' && ['paper', 'lizard'].includes(cChoice)) ||
+  (uChoice === 'lizard' && ['paper', 'spock'].includes(cChoice)) ||
+  (uChoice === 'spock' && ['rock', 'scissors'].includes(cChoice));
+}
+
+function determineWinnerOfSingleGame(uChoice, cChoice) {
+  if (uChoice === cChoice) {
+    return 'tie';
+  } else if (userWins(uChoice, cChoice)) {
+    return 'user';
+  } else return 'computer';
 }
 
 function userWantsAnotherGame() {
-	let answer = prompt(MESSAGES.anotherGame).toLowerCase();
-	while (answer[0] !== 'n' && answer[0] !== 'y') {
-		answer = prompt(MESSAGES.invalidRequestForAnotherGame).toLowerCase();
-	}
-	console.clear();
-	if (answer[0] !== 'y') return false;
-	else return true;
-};
+  let answer = prompt(MESSAGES.anotherGame).toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    answer = prompt(MESSAGES.invalidRequestForAnotherGame).toLowerCase();
+  }
 
-while (true) {
-	let usersChoice = getUsersChoice(MESSAGES.chooseRPS);
+  console.clear();
 
-	while (isInvalid(usersChoice)) {
-		usersChoice = getUsersChoice(MESSAGES.invalidChoice);
-	}
-
-	let compChoice = getCompChoice();
-
-	console.log(`You chose ${usersChoice} | the computer chose ${compChoice}.`);
-	
-	let outcome = determineWinner(usersChoice, compChoice);
-	
-	console.log(outcome);
-
-	if (!userWantsAnotherGame()) break;
+  if (answer[0] !== 'y') return false;
+  else return true;
 }
 
-console.log('Thanks for playing!')
+function incrementScore(sb, winner) {
+  if (winner === 'user') {
+    sb.user++;
+  } else if (winner === 'computer') {
+    sb.computer++;
+  }
+}
+
+function checkForWinner(sb) {
+  if (sb.user === 3) {
+    return [true, 'You win!'];
+  } else if (sb.computer === 3) {
+    return [true, 'Computer wins...'];
+  } else {
+    return [false];
+  }
+}
+
+function getAndValidateUsersChoice() {
+  let usersChoice = getUsersChoice(MESSAGES.chooseRPS);
+
+  while (isInvalid(usersChoice)) {
+    usersChoice = getUsersChoice(MESSAGES.invalidChoice);
+  }
+
+  return usersChoice;
+}
+
+function displayCurrentScoreboard(sb) {
+  console.log(`You: ${sb.user}\nComputer: ${sb.computer}\n`);
+}
+
+function displaySingleGamePlays(uChoice, cChoice) {
+  console.log(`You chose ${uChoice} | the computer chose ${cChoice}.`);
+}
+
+function playGame(sb) {
+  while (true) {
+    let usersChoice = getAndValidateUsersChoice();
+    let compChoice = getCompChoice();
+
+    displaySingleGamePlays(usersChoice, compChoice);
+    let winnerOfSingleGame = determineWinnerOfSingleGame(
+      usersChoice,
+      compChoice
+    );
+
+    incrementScore(sb, winnerOfSingleGame);
+    displayCurrentScoreboard(sb);
+
+    let [isWinner, winner] = checkForWinner(sb);
+    if (isWinner) {
+      console.log(winner);
+      break;
+    }
+  }
+}
+
+function startGame() {
+  let scoreboard = {
+    user: 0,
+    computer: 0
+  };
+
+  playGame(scoreboard);
+}
+
+while (true) {
+
+  startGame();
+
+  if (!userWantsAnotherGame()) break;
+}
+
+console.log('Thanks for playing!');
